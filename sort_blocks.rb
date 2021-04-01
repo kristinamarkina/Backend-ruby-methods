6daa62d4f8617ba578c4e61d7612996dae37b586
Kristina Markina <kr.markina@gmail.com>
2021-04-01 17:38:08 -0400

    Update sort_blocks.rb
    Add additional calls to book_array.sort that pass blocks. For your first call to sort, pass a block so that the array is sorted in order of title, and print out the array. For your second call to sort, pass a block so that the array is sorted in order of copies, and print out the array.

--- sort_blocks.rb
+++ sort_blocks.rb
@@ -15,18 +15,26 @@ book_array.push(Book.new("Beatrice Potter","Peter Rabbit",25))
 book_array.push(Book.new("Henry Fielding","Tom Jones",12))
 book_array.push(Book.new("Bob Woodward","All the President's Men",30))
 
-puts "Sorting alphabetically by author"
-
+puts "\nSorting alphabetically by author"
 new_array = book_array.sort do |a,b|
   author1 = a.author.downcase
   author2 = b.author.downcase
   author1 <=> author2
-  # if author1 > author2
-  #   1
-  # elsif author1 < author2
-  #   -1
-  # else
-  #   0
-  # end
+end
+puts new_array
+
+puts "\nSorting alphabetically by title"
+new_array = book_array.sort do |a,b|
+  title1 = a.title.downcase
+  title2 = b.title.downcase
+  title1 <=> title2
+end
+puts new_array
+
+puts "\nSorting by copies"
+new_array = book_array.sort do |a,b|
+  count1 = a.count
+  count2 = b.count
+  count1 <=> count2
 end
 puts new_array
